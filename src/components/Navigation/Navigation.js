52ea0de29502ab7d689f7670ebe03c1b9f34c66a
thedoft <thedoft@gmail.com>
2021-03-03 13:31:58 +0300

    feat: do dinamic menu change

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Menu from '../Menu/Menu';
 import MobileMenu from '../MobileMenu/MobileMenu';
 
-const Navigation = () => (
-  <>
-    {
-      (window.innerWidth > 768) ? <Menu /> : <MobileMenu />
-    }
-  </>
-);
+const Navigation = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
+  const updateWindowSize = () => setWindowWidth(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener('resize', updateWindowSize);
+
+    return () => window.removeEventListener('resize', updateWindowSize);
+  });
+
+  return (
+    <>
+      {
+        (windowWidth > 768) ? <Menu /> : <MobileMenu />
+      }
+    </>
+  );
+};
 export default Navigation;
