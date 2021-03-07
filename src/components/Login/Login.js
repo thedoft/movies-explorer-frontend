55d1419787727ba076b14b77c898436122c7dc07
thedoft <thedoft@gmail.com>
2021-03-07 13:50:22 +0300

    feat: refactor and validate login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,19 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Header from '../Header/Header';
 import Section from '../Section/Section';
 import Form from '../Form/Form';
+import useFormWithValidation from '../../hooks/useFormWithValidation';
 
 const Login = ({ onLogin }) => {
-  const [userData, setUserData] = useState({
-    email: '',
-    password: '',
-  });
+  const {
+    values, handleChange, errors, isValid,
+  } = useFormWithValidation({ name: '', email: '', password: '' });
 
-  const handleChange = (evt) => {
-    setUserData({
-      ...userData,
-      [evt.target.name]: evt.target.value,
-    });
+  const handleFocus = (evt) => {
+    evt.target.removeAttribute('readonly');
   };
 
   return (
@@ -25,14 +22,18 @@ const Login = ({ onLogin }) => {
           caption="Еще не зарегистрированы? "
           linkPath="/signup"
           linkText="Регистрация"
-          data={userData}
+          data={values}
           onSubmit={onLogin}
+          isValid={isValid}
         >
           <fieldset className="form__fieldset">
             <label className="form__label" htmlFor="email">E-mail</label>
-            <input name="email" value={userData.email} onChange={handleChange} type="email" className="form__input" id="email" required autoComplete="current-email" />
+            <input name="email" value={values.email} onChange={handleChange} type="email" className="form__input" id="email" required autoComplete="current-email" pattern="^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" readOnly onFocus={handleFocus} />
+            <span className="form__error">{errors.email}</span>
+
             <label className="form__label" htmlFor="password">Пароль</label>
-            <input name="password" value={userData.password} onChange={handleChange} type="password" className="form__input" id="password" required autoComplete="current-password" />
+            <input name="password" value={values.password} onChange={handleChange} type="password" className="form__input" id="password" required minLength={4} autoComplete="current-password" />
+            <span className="form__error">{errors.password}</span>
           </fieldset>
         </Form>
       </Section>
